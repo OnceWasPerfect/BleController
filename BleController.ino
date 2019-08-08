6dcab18ba4e3bc8e686bd8fd94adbf1f54b1b3e6
OnceWasPerfect <oncewasperfect@gmx.com>
2019-08-08 09:02:27 -0400

    initial commit

--- BleController.ino
+++ BleController.ino
@@ -2,8 +2,8 @@
 #include <Wire.h>
 #include <PushButton.h>
 #include <SPI.h>
-#include <Adafruit_LIS3DH.h>
-#include <Adafruit_Sensor.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
 #define RANGE 7  //How far the mouse moves
 #define RESPONSEDELAY 5  //How often the loop runs
@@ -12,34 +12,37 @@
 #define DEADZONE 200  //How far before movement registered
 #define MAINBUTTONPIN 27 //Pin for main button
 #define SCROLLBUTTONPIN 30 //Pin for scroll button
+#define RADIOCEPIN 28  //Controller enable pin for radio
+#define RADIOCSNPIN 29  //Controller select pin for radio
 
+//Button setup
 PushButton mainButton(MAINBUTTONPIN);  //Main button
 PushButton scrollButton(SCROLLBUTTONPIN);  //Scroll button
-Adafruit_LIS3DH lis = Adafruit_LIS3DH();  //Create acc object
+bool bolScroll = false;  //Is scroll mode active
+
+//Accelerometer data setup
 int xCalibrated = 0;  //Base value for x
 int yCalibrated = 0;  //Base value for y
 int xDistance = 0;  //Movement of x axis
 int yDistance = 0;  //Movement of y axis
-bool bolScroll = false;  //Is scroll mode active
+
+//Radio setup
+RF24 radio(RADIOCEPIN, RADIOCSNPIN); // create radio object CE, CSN
+const byte address[6] = "00001";  //Address the radios will use
 
 void setup()
 {
-  //Assign button pins
-  pinMode(MAINBUTTONPIN, INPUT_PULLUP);
-  pinMode(SCROLLBUTTONPIN, INPUT_PULLUP);
-
-  //Set buttons to active low
-  scrollButton.setActiveLogic(LOW);
-  mainButton.setActiveLogic(LOW);
-
-  //Begin acc
-  lis.begin(0x18);
+  //Button setup
+  pinMode(MAINBUTTONPIN, INPUT_PULLUP);  //Set button pin with pullup
+  pinMode(SCROLLBUTTONPIN, INPUT_PULLUP);  //Set button pin with pullup
+  scrollButton.setActiveLogic(LOW);  //Make button active low
+  mainButton.setActiveLogic(LOW);  //Make button active low
   
-  //Set RANGE for Acc
-  lis.setRange(LIS3DH_RANGE_16_G);   // 2, 4, 8 or 16 G!
-
-  //Calibrate X and Y axis
-  calibrate();
+  //Setup radio
+  radio.begin();  //Start radio object
+  radio.openReadingPipe(0, address);  //The pipe and address it will listen on
+  radio.setPALevel(RF24_PA_MIN);  //The power level it is using
+  radio.startListening();  //Start listening for data
   
   //Start bluetooth
   initializeBluefruit();
@@ -102,11 +105,15 @@ int averageX ()
 {
   int average = 0;
   long total = 0L;
+  int location[] = {0,0};
 
   for(int i = 0; i < AVERAGEFACTOR; i++)
   {
-    lis.read();  //Get new reading from acc
-    total = total + lis.x;  //Sum the readings
+    if (radio.available()) 
+    {
+      radio.read(&location, sizeof(location));
+    }
+    total = total + location[0];  //Sum the readings
   }
   
   average = total / AVERAGEFACTOR;  //Get the average
@@ -118,11 +125,15 @@ int averageY ()
 {
   int average = 0;
   long total = 0L;
+  int location[] = {0,0};
 
   for(int i = 0; i < AVERAGEFACTOR; i++)
   {
-    lis.read();  //Get new reading from acc
-    total = total + lis.y;  //Sum the readings
+    if (radio.available()) 
+    {
+      radio.read(&location, sizeof(location));
+    }
+    total = total + location[1];  //Sum the readings
   }
   
   average = total / AVERAGEFACTOR;  //Get the average
@@ -134,12 +145,16 @@ void calibrate ()
 {
   long xTotal = 0L;
   long yTotal = 0L;
+  int location[] = {0,0};
 
   for (int i = 0; i < CALIBRATIONFACTOR; i++)  //Sample location CALIBRATIONFACTOR times
   {
-    lis.read();  //Get new reading from acc
-    xTotal = xTotal + lis.x;  //Total the x locations
-    yTotal = yTotal + lis.y;  //Total the y locations
+    if (radio.available()) 
+    {
+      radio.read(&location, sizeof(location));
+    }
+    xTotal = xTotal + location[0];  //Total the x locations
+    yTotal = yTotal + location[1];  //Total the y locations
   }
 
   xCalibrated = xTotal / CALIBRATIONFACTOR; //Average the x location
