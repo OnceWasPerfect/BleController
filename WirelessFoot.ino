fbf622819c623347e651bf27a1763ed5d3b2fd38
OnceWasPerfect <oncewasperfect@gmx.com>
2019-08-10 07:56:33 -0400

    setup debug

--- WirelessFoot.ino
+++ WirelessFoot.ino
@@ -4,6 +4,18 @@
 #include <nRF24L01.h>  //Wireless tranceiver library
 #include <RF24.h>  //Wireless tranceiver library
 
+//Debug setup
+#define DEBUG //comment out to disable debug
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+ #define DEBUG_BEGIN(x)     Serial.begin(x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTLN(x) 
+ #define DEBUG_BEGIN(x)
+#endif
+
 #define RADIOCEPIN 10  //Chip enable pin for radio
 #define RADIOCSNPIN 9 //Chip select pin for radio
 
@@ -17,6 +29,7 @@ const byte address[6] = "00001";  //The address the radio will work on
 
 void setup()
 {
+  DEBUG_BEGIN(115200);
   //Accelerometer setup 
   lis.begin(0x18);  //Start the accelerometer object
   lis.setRange(LIS3DH_RANGE_16_G);  //Set RANGE for accelerometer 2, 4, 8 or 16 G!
@@ -37,7 +50,9 @@ void loop()
 
     //Send data via radio
     radio.write(&location, sizeof(location)); 
+    DEBUG_PRINT("x = "); DEBUG_PRINTLN(location[0]);
+    DEBUG_PRINT("y = "); DEBUG_PRINTLN(location[1]);
     
-    delay(500);  //small delay to let it finish
+    delay(10);  //small delay to let it finish
 }
   
